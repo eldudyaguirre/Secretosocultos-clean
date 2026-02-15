630fe48453d38bdafd40dabdb2c98b70f6b647eb
Adrián Aguirre <eldudyaguirre@gmail.com>
2026-02-14 19:48:51 -0500

    Fix Pillow version for Render Free

--- build.sh
+++ build.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 set -o errexit
 
-pip install --upgrade pip setuptools wheel
+pip install --upgrade pip
+
 pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
