6dafd7fb123625cf3bd017d31e7cfd4a4a894e16
Adrián Aguirre <eldudyaguirre@gmail.com>
2026-02-14 21:21:45 -0500

    Fix Railway database config

--- jdante/settings.py
+++ jdante/settings.py
@@ -88,14 +88,9 @@ WSGI_APPLICATION = 'jdante.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.environ.get('DB_NAME'),
-        "USER": os.environ.get('DB_USER'),
-        "PASSWORD": os.environ.get('DB_PASSWORD'),
-        "HOST": os.environ.get('DB_HOST'),
-        "PORT": os.environ.get('DB_PORT'),
-    }
+    "default": dj_database_url.config(
+        default=os.environ.get("DATABASE_URL")
+    )
 }
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
