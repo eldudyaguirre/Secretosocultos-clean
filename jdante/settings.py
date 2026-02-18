d133c99b7a3a5c71973fb85ae375c265958b5d9c
Adrián Aguirre <eldudyaguirre@gmail.com>
2026-02-18 17:52:24 -0500

    arreglo faq

--- jdante/settings.py
+++ jdante/settings.py
@@ -103,6 +103,8 @@ DATABASES = {
         default=os.environ.get("DATABASE_URL")
     )
 }
+
+
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
